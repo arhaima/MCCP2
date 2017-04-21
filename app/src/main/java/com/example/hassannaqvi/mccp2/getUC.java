1047332eeae205f40c8a34a389b118165af55e55
gul.sanober <gulsanobermemon@gmail.com>
2017-04-21 15:25:16 +0500

    MCCP changes

--- app/src/main/java/com/example/hassannaqvi/mccp2/getUC.java
+++ app/src/main/java/com/example/hassannaqvi/mccp2/getUC.java
@@ -34,7 +34,7 @@ public class getUC extends AsyncTask<String, String, String> {
         StringBuilder result = new StringBuilder();
 
         try {
-            URL url = new URL(MCCP2App.HOST_URL + "uc_array.php");
+            URL url = new URL(MCCP2App.HOST_URL);
             urlConnection = (HttpURLConnection) url.openConnection();
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
