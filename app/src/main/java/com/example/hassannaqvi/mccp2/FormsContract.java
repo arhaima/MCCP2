e2dd929e15e76f14f930f78dadd805792b17081b
shznaqvi <shznaqvi@gmail.com>
2017-05-22 10:57:36 +0500

    changed age limit in IM section to include 2017

--- app/src/main/java/com/example/hassannaqvi/mccp2/FormsContract.java
+++ app/src/main/java/com/example/hassannaqvi/mccp2/FormsContract.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
  */
 public class FormsContract {
     private static final String TAG = "FORM_CONTRACT";
-    private static final String PROJECT_NAME = "MCCP2-Baseline-Phase-III";
+    private static final String PROJECT_NAME = "MCCP3-Baseline-Phase-III";
     private static final String SURVEY_TYPE = "1";
     //private static FormsContract fcInstance = null;
     private Long _ID;
